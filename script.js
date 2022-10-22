6a90fce6e63fb134777db3628f31b15fdf7d7a2f
Benson Njuguna <njugunab655@gmail.com>
2022-10-22 20:05:28 +0300

    Select dom elements and declare variables

---
+++ script.js
@@ -0,0 +1,13 @@
+(function() {
+    const gameCells = Array.from(document.querySelectorAll('.cell'));
+    const board = document.querySelector('#board');
+    const winningMessageElement = document.querySelector('.winningMessage');
+    const winningMessage = document.querySelector('.message');
+    const playerX = document.querySelector('#playerX').value;
+    const playerO = document.querySelector('#playerO').value;
+    const restartButton = document.querySelector('#restartButton');
+    const X_CLASS = 'x';
+    const CIRCLE_CLASS = 'circle';
+    let currentClass;
+
+})();
