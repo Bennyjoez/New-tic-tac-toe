1eff0a19bae0958bb3a3b8dcda91feb73b73a794
Benson Njuguna <njugunab655@gmail.com>
2022-10-22 22:46:07 +0300

    Determine winner and display winning message

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 (function() {
     const gameCells = Array.from(document.querySelectorAll('.cell'));
     const board = document.querySelector('#board');
+    const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
     const winningMessageElement = document.querySelector('.winningMessage');
     const winningMessage = document.querySelector('.message');
     const playerX = document.querySelector('#playerX').value;
@@ -14,28 +24,52 @@
     function startGame() {
         currentPlayer = playerX;
         gameCells.forEach(cell => {
-            cell.addEventListener('click', handleClick)
+            cell.addEventListener('click', handleClick, {once:true})
         });
 
         board.classList.add('x')
     }
 
-    function handleClick(e) {        
+    function handleClick(e) {    
+        let currentClass;  
+        if(currentPlayer === playerX) {
+            currentClass = X_CLASS;
+        } else {
+            currentClass = CIRCLE_CLASS;
+        }
         placeMark(e, currentPlayer);
-        changePlayer();
+
+        if(checkWin(currentClass)) {
+            endGame(false);
+        } else if(isDraw()){
+            endGame(true);
+        } else {
+            changePlayer();
+        };
+    }
+
+    function endGame(draw) {
+        if(draw) {
+            winningMessage.innerText = "Draw!"
+        } else {
+            winningMessage.innerText = `${(currentPlayer === playerX) ? playerX : playerO} Wins!`;
+            winningMessage.style.textTransform = "capitalize";
+        }
+        winningMessageElement.classList.add('show');
+    }
+
+    function isDraw() {
+        return [...gameCells].every(cell => {
+            return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+        })
     }
 
     function placeMark(e, currentPlayer) {
-        // board.classList.remove(X_CLASS);
-        // board.classList.remove(CIRCLE_CLASS);
         if(currentPlayer === playerX) {
             e.target.classList.add(X_CLASS);
-            // board.classList.add(CIRCLE_CLASS)
         } else if (currentPlayer === playerO) {
             e.target.classList.add(CIRCLE_CLASS)
-            // board.classList.add(X_CLASS)
         }
-
     }
 
     function changePlayer() {
@@ -48,5 +82,13 @@
             currentPlayer = playerX;
             board.classList.add(X_CLASS);
         }
+    }
+
+    function checkWin (currentClass) {
+        return winningCombinations.some(combination => {
+            return combination.every(index => {
+                return gameCells[index].classList.contains(currentClass);
+            })
+        })
     }
 })();
